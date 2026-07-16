19b5543901ae052d9a59fcff8272d993084590e6
Silaakbulut <silaakbltt@gmail.com>
2026-07-16 12:07:46 +0300

    refactor: update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,73 @@
 <?php
 
-use App\Http\Controllers\SayfaController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SayfaController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GunlukController;
 
-Route::get('/', function () {return view('welcome');});
 
-// Sayfalar
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/hakkimizda', [SayfaController::class, 'hakkimizda']);
-
 Route::get('/iletisim', function () {
-    return view('iletisim');});
-
+    return view('iletisim');
+});
 Route::get('/staj', function () {
-    return view('staj');});
-
+    return view('staj');
+});
 Route::get('/ogrenciler', [SayfaController::class, 'ogrenciler']);
 
-Route::get('/register', [AuthController::class, 'register']);
-Route::post('/register', [AuthController::class, 'registerPost']);
 
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'loginPost']);
-Route::post('/logout', [AuthController::class, 'logout']);
-Route::get('/AnaSayfa', function () {
-    return view('anasayfa');
-})->middleware('auth');
-Route::get('/profil', [AuthController::class, 'profil'])->middleware('auth');
-Route::post('/profil', [AuthController::class, 'profilGuncelle'])->middleware('auth');
-Route::get('/gunlukler', [GunlukController::class, 'index']);
-Route::get('/gunlukler/create', [GunlukController::class, 'create']);
 
-Route::post('/gunlukler', [GunlukController::class, 'store']);
-Route::get('/gunlukler/{id}/edit', [GunlukController::class, 'edit']);
-Route::put('/gunlukler/{id}', [GunlukController::class, 'update']);
-Route::delete('/gunlukler/{id}', [GunlukController::class, 'destroy']);
+Route::controller(AuthController::class)
+    ->middleware('guest')
+    ->group(function () {
+
+        Route::get('/login', 'login')->name('login');
+        Route::post('/login', 'loginPost');
+
+        Route::get('/register', 'register')->name('register');
+        Route::post('/register', 'registerPost');
+
+    });
+
+
+
+Route::controller(AuthController::class)
+    ->middleware('auth')
+    ->group(function () {
+
+        Route::post('/logout', 'logout')->name('logout');
+
+        Route::get('/AnaSayfa', function () {
+            return view('anasayfa');
+        });
+
+        Route::get('/profil', 'profil');
+        Route::post('/profil', 'profilGuncelle');
+
+    });
+
+
+
+
+Route::prefix('gunlukler')
+    ->name('gunlukler.')
+    ->middleware('auth')
+    ->controller(GunlukController::class)
+    ->group(function () {
+
+        Route::get('/', 'index')->name('index');
+
+        Route::get('/create', 'create')->name('create');
+        Route::post('/', 'store')->name('store');
+
+        
+        Route::get('/{id}/edit', 'edit')->name('edit');
+        Route::put('/{id}', 'update')->name('update');
+        Route::delete('/{id}', 'destroy')->name('destroy');
+
+    });
